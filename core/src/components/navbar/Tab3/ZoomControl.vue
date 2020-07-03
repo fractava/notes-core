dbbad3c9552b5a764b247590fb332b350b4e3167
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2020-07-03 17:14:37 +0200

    Added "reset zoom" feature to View tab featuring a placeholder icon

--- core/src/components/navbar/Tab3/ZoomControl.vue
+++ core/src/components/navbar/Tab3/ZoomControl.vue
@@ -6,6 +6,9 @@
 		<md-button v-on:click="minus">
 			<md-icon>remove</md-icon>
 		</md-button>
+		<md-button v-on:click="reset">
+			<md-icon>highlight_alt</md-icon>
+		</md-button>
 	</div>
 </template>
 
@@ -19,6 +22,9 @@ export default {
 		},
 		minus: function() {
 			this.$store.commit("setScale", {scale: this.loadedPage.scale - 0.1,}, {module: "core" });
+		},
+		reset: function() {
+			this.$store.commit("setScale", {scale: 1,}, {module: "core" });
 		},
 	},
 	computed: mapState({
