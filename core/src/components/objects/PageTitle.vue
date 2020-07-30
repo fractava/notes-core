a2606f1e07278bea316a16dadb837ac8c20f5ad3
Jonathan Treffler <mail@jonathan-treffler.de>
2020-07-30 17:08:46 +0200

    improved Page Title Position and Size

--- core/src/components/objects/PageTitle.vue
+++ core/src/components/objects/PageTitle.vue
@@ -5,10 +5,11 @@
         v-on:pointermove="stopPropagation"
         v-on:pointerup="stopPropagation"
         v-on:pointerleave="stopPropagation"
+        :style="{'top': loadedPage.background.size + 1  + 'px', 'left': loadedPage.background.size + 1 + 'px', 'height': inputSize   + 'px', 'min-width': loadedPage.background.size * 5 - 1 + 'px'}"
     >
         <md-field
             class="pageTitle"
-            :style="{top: navbarHeight+'%'}"
+            md-inline
         >
         <label>Title</label>
         <md-input v-model="title"></md-input>
@@ -36,9 +37,19 @@ export default {
 				return this.loadedPage.title;
 			}
 		},
+    inputSize: function() {
+      let size = this.loadedPage.background.size;
+
+      while(size < 32) {
+        size = size + this.loadedPage.background.size;
+      }
+
+      return size;
+    },
 		...mapState({
 			loadedPage: state => state.core.loadedPage,
 			navbarHeight: state => state.core.navbarHeight,
+      loadedPage: state => state.core.loadedPage,
 		}),
 	},
 };
@@ -48,8 +59,16 @@ export default {
 .pageTitleContainer {
     position: absolute;
     left: 0px;
-    margin-top: 20px;
-    margin-left: 20px;
     z-index: 2;
+    display: grid;
+    place-items: end;
+    transition: all 1s ease 0s;
+    background: white;
+}
+.pageTitle {
+  margin: 0px;
+  width: 100%;
+  position: absolute;
+  bottom: 0px;
 }
 </style>
