c6ce26a1a0041c3b9fd1563e11b52126c09674ad
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2023-03-24 19:15:20 +0000

    fixed dev server websocket

--- dev-server/vue.config.js
+++ dev-server/vue.config.js
@@ -1,6 +1,10 @@
 module.exports = {
 	devServer: {
-		allowedHosts: "all"
+      allowedHosts: "all",
+      host: '0.0.0.0',
+      client: {
+        webSocketURL: 'auto://0.0.0.0:0/ws',
+      }
     },
     publicPath: process.env.NODE_ENV === 'production'
         ? '/notes-core/'
