db2aa0020a9278b5f5df6b5cace52de95dc4293e
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2023-03-26 12:34:32 +0000

    installed vuetify

--- dev-server/vite.config.ts
+++ dev-server/vite.config.ts
@@ -3,10 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import vuetify from 'vite-plugin-vuetify'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vuetify({ autoImport: true }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
